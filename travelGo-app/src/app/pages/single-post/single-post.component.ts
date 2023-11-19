14372707b7e32671732b74e9332fc01e25d62ec6
Wiktoria Lewicka <wiktorialewicka258@gmail.com>
2023-11-19 19:47:59 +0100

    Add HTML support in posts & small fixes

--- travelGo-app/src/app/pages/single-post/single-post.component.ts
+++ travelGo-app/src/app/pages/single-post/single-post.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PostCard} from "../../types/post-card";
 import {PostsService} from "../../services/posts.service";
 import {ActivatedRoute} from "@angular/router";
